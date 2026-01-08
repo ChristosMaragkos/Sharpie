583cfd60935dd8c44661b0b11e6e811ff3064dd1
Christos Maragkos <whitetowersoftware@gmail.com>
2026-01-08 05:08:51 +0200

    Expand Input Handler driver to allow better control

--- src/Sharpie.Core/Drivers/IInputHandler.cs
+++ src/Sharpie.Core/Drivers/IInputHandler.cs
@@ -1,4 +1,27 @@
-public interface IInputHandler
+public abstract class InputHandler
 {
-    byte[] GetInputState();
+    public abstract (byte, byte) GetInputState();
+
+    protected void AddKeyToState(ref byte controllerState, ControllerKeys key) =>
+        controllerState |= (byte)key;
+
+    protected bool IsButtonStateOn(byte controllerState, ControllerKeys button)
+    {
+        var buttonVal = (byte)button;
+        var buttonOn = controllerState & buttonVal;
+        return buttonOn == buttonVal;
+    }
+
+    [Flags]
+    protected enum ControllerKeys : byte
+    {
+        Up = 0x01,
+        Down = 0x02,
+        Left = 0x04,
+        Right = 0x08,
+        ButtonA = 0x10,
+        ButtonB = 0x20,
+        ButtonStart = 0x40,
+        ButtonOption = 0x80,
+    }
 }
