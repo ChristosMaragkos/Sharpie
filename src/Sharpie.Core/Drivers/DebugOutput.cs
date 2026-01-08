5028a7c7770b7b4693ef8606f512a2be12469309
Christos Maragkos <whitetowersoftware@gmail.com>
2026-01-08 05:07:07 +0200

    Add debug output handler and OUT opcode family for debugging from within assembly

---
+++ src/Sharpie.Core/Drivers/DebugOutput.cs
@@ -0,0 +1,32 @@
+using System.Collections.Concurrent;
+
+public abstract class DebugOutput
+{
+    protected ConcurrentQueue<string> MessageQueue = new();
+    private readonly int _size;
+
+    protected DebugOutput(int size)
+    {
+        _size = size;
+    }
+
+    public void PushDebug(string message)
+    {
+        if (MessageQueue.Count == _size)
+            MessageQueue.TryDequeue(out _);
+
+        MessageQueue.Enqueue(message);
+    }
+
+    public void LogAll()
+    {
+        while (!MessageQueue.IsEmpty)
+        {
+            MessageQueue.TryDequeue(out string? message);
+            if (message != null)
+                Log(message);
+        }
+    }
+
+    public abstract void Log(string message);
+}
