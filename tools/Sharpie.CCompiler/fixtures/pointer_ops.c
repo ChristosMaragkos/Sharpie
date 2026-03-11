3c491b884dc343938ee1e9dc34a62309683de865
Christos Maragkos <whitetowersoftware@gmail.com>
2026-03-11 10:56:36 +0200

    Why does Rider keep leaving whitespace?

--- tools/Sharpie.CCompiler/fixtures/pointer_ops.c
+++ tools/Sharpie.CCompiler/fixtures/pointer_ops.c
@@ -1,11 +1,11 @@
 int main(void) {
-    // int x = 500; TODO: This requires addressing logic so I'll implement it later.
-    // int* p = &x;
-    
-    // Manual pointer math
-    int* ptr = (int*)1000;
-    *ptr = 42;       // STA
-    int val = *ptr;  // LDP
-    
-    return val;
+  // int x = 500; TODO: This requires addressing logic so I'll implement it
+  // later. int* p = &x;
+
+  // Manual pointer math
+  int *ptr = (int *)1000;
+  *ptr = 42;      // STA
+  int val = *ptr; // LDP
+
+  return val;
 }
