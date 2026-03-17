124ee3b53426bbccea0ab6d0ec41678d9c31a479
Christos Maragkos <whitetowersoftware@gmail.com>
2026-03-17 21:49:57 +0200

    Add asm fixture

---
+++ tools/Sharpie.CCompiler/fixtures/logic/inline_asm.asm
@@ -0,0 +1,10 @@
+.REGION FIXED
+Main:
+    PUSH r15
+    GETSP r15
+    LDI r1, 0 ; This should exist in the method.
+    LDI r0, 0
+    SETSP r15
+    POP r15
+    HALT
+.ENDREGION
---
+++ tools/Sharpie.CCompiler/fixtures/logic/inline_asm.c
@@ -0,0 +1,4 @@
+int main(void) {
+  asm("LDI r1, 0 ; This should exist in the method.");
+  return 0;
+}
