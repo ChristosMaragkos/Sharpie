f3b4e455123eb9427e8cad538d164c5a3cc27c43
Christos Maragkos <whitetowersoftware@gmail.com>
2026-03-28 18:48:19 +0200

    Add clock

---
+++ editor/Scripts/Clock.cs
@@ -0,0 +1,27 @@
+using Godot;
+
+namespace SharpieStudio;
+
+public partial class Clock : PanelContainer
+{
+    [Export]
+    public Label DateLabel { get; private set; }
+
+    [Export]
+    public Label TimeLabel { get; private set; }
+
+    public override void _Process(double delta)
+    {
+        var date = Time.GetDateDictFromSystem(); // YY/MM/DD/Weekday
+        var time = Time.GetTimeDictFromSystem(); // HH/MM/SS
+
+        DateLabel.Text = $"{date["day"]}/{date["month"]}/{date["year"]}";
+        TimeLabel.Text =
+            $"{time["hour"]}:{time["minute"]}:{FormatSeconds(time["second"].AsString())}";
+    }
+
+    public static string FormatSeconds(string seconds)
+    {
+        return int.Parse(seconds) < 10 ? $"0{seconds}" : seconds;
+    }
+}
