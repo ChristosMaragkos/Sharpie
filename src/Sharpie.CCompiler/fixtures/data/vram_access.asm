e17b85d2667e1110c7a6593bffddf0b7f128f411
Christos Maragkos <whitetowersoftware@gmail.com>
2026-05-18 23:37:30 +0300

    Add fixtures for VRAM access

---
+++ src/Sharpie.CCompiler/fixtures/data/vram_access.asm
@@ -0,0 +1,40 @@
+; ------------------------
+; Sharpie C cartridge
+; ------------------------
+.REGION FIXED
+    JMP Main
+.ENDREGION
+; ----------------------------------
+; SOURCE: vram_access.c
+; ----------------------------------
+
+.REGION FIXED
+.GLOBAL
+Main:
+    PUSH r8
+    PUSH r9
+    LDI r1, 3
+    BLITMODE r1
+    LDI r9, 1
+    XOR r1, r1
+    MOV r8, r1
+    LDI r1, 32639
+    MOV r2, r9
+    STV r1, r2
+while_start_L1:
+    MOV r1, r8
+    ICMP r1, 0
+    JNE while_end_L2
+    XOR r1, r1
+    INPUT r1, r0
+    MOV r8, r0
+    JMP while_start_L1
+while_end_L2:
+    XOR r0, r0
+epilogue_L0:
+    POP r9
+    POP r8
+    HALT
+.ENDGLOBAL
+.ENDREGION
+
