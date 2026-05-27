3ee62a8793ef354be858b93346362fcf54948311
Christos Maragkos <whitetowersoftware@gmail.com>
2026-05-27 07:57:01 +0300

    Re-do function fixtures

--- src/Sharpie.CCompiler/fixtures/functions/function_pointers.c
+++ src/Sharpie.CCompiler/fixtures/functions/function_pointers.c
@@ -3,17 +3,22 @@ int add(int a, int b) { return a + b; }
 int sub(int a, int b) { return a - b; }
 
 int do_math(int (*operation)(int, int), int x, int y) {
-  return operation(x, y);
+    return operation(x, y);
 }
 
+const int expected_result = 20;
+
 int main(void) {
-  int (*ops[2])(int, int); // array of function pointers
+    int (*ops[2])(int, int);
+    ops[0] = add;
+    ops[1] = sub;
 
-  ops[0] = add;
-  ops[1] = sub;
+    int res1 = do_math(add, 10, 5);
+    int res2 = do_math(ops[1], 10, 5);
 
-  int res1 = do_math(add, 10, 5);
-  int res2 = do_math(ops[1], 10, 5);
+    int actual_result = res1 + res2;
+    if (expected_result != actual_result)
+        return 1;
 
-  return res1 + res2;
+    return 0;
 }
--- src/Sharpie.CCompiler/fixtures/functions/recursive_functions.c
+++ src/Sharpie.CCompiler/fixtures/functions/recursive_functions.c
@@ -1,8 +1,12 @@
 int factorial(int number) {
-  if (number == 1)
-    return 1;
-  else
-    return (number * factorial(number - 1));
+    if (number == 1)
+        return 1;
+    else
+        return (number * factorial(number - 1));
 }
 
-int main(void) { return factorial(5); }
+int main(void) {
+    int result = factorial(5);
+    if (result != 120) return 1;
+    return 0;
+}
