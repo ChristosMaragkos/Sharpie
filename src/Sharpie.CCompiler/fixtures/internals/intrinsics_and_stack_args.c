0b0d815b4bb63e6b51ab579d4e6d9215a487d89d
Christos Maragkos <whitetowersoftware@gmail.com>
2026-05-27 07:57:06 +0300

    Re-do internals fixtures

--- src/Sharpie.CCompiler/fixtures/internals/intrinsics_and_stack_args.c
+++ src/Sharpie.CCompiler/fixtures/internals/intrinsics_and_stack_args.c
@@ -1,23 +1,11 @@
 #include "../headers/sharpie.h"
 
 int add_six_numbers(int a, int b, int c, int d, int e, int f) {
-  return a + b + c + d + e + f;
+    return a + b + c + d + e + f;
 }
 
-int test_memory() {
-  int *buffer = alloca(sizeof(int) * 10);
-
-  memset(buffer, 255, sizeof(int) * 10);
-
-  return 1; // We haven't implemented array indexing yet, so just
-            // returning 1 is fine
-}
-
-int main() {
-  int sum = add_six_numbers(1, 2, 3, 4, 5, 6);
-
-  clear_screen(0);
-  draw_sprite(10, 20, 5, ATTR_HFLIP | ATTR_ALTPAL, 2);
-
-  return sum;
+int main(void) {
+    int sum = add_six_numbers(1, 2, 3, 4, 5, 6);
+    if (sum != 21) return 1;
+    return 0;
 }
