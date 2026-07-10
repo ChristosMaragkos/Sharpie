3f910fae538abe93c550a575fe7249c334129d87
Christos Maragkos <whitetowersoftware@gmail.com>
2026-07-10 07:33:12 +0300

    Add arena allocator header and impl file for dynamic allocations

---
+++ src/Sharpie.CCompiler/libs/sharpie_mem.c
@@ -0,0 +1,43 @@
+#include "sharpie_mem.h"
+#include "sharpie.h"
+
+struct ArenaAllocator {
+    void *data;
+    size_t offset;
+    size_t size;
+};
+
+void arena_init(ArenaAllocator *arena, void *backing, size_t size) {
+    arena->data = backing;
+    arena->size = size;
+    arena->offset = 0;
+}
+
+void *arena_malloc(ArenaAllocator *arena, size_t size) {
+    void *ptr = arena->data + arena->offset;
+    if (arena->offset + size > arena->size) {
+        return NULL;
+    } else {
+        arena->offset += size;
+        return ptr;
+    }
+}
+
+void *arena_alloc_zero(ArenaAllocator *arena, size_t size) {
+    void *ptr = arena->data + arena->offset;
+    if (arena->offset + size > arena->size) {
+        return NULL;
+    } else {
+        memset(ptr, 0, size);
+        arena->offset += size;
+        return ptr;
+    }
+}
+
+void arena_free_all(ArenaAllocator *arena) { arena->offset = 0; }
+
+ArenaSnapshot arena_snapshot(ArenaAllocator *arena) { return arena->offset; }
+
+void arena_restore(ArenaAllocator *arena, ArenaSnapshot snapshot) {
+    arena->offset = snapshot;
+}
